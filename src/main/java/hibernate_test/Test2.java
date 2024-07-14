87a7b210b7a30c362958261195e6b5addca72236
dimakhodosevich <dimakhodosevich95@gmail.com>
2024-07-14 12:41:00 +0300

    Get all objects from DB using query (hibernate query language)

---
+++ src/main/java/hibernate_test/Test2.java
@@ -0,0 +1,31 @@
+package hibernate_test;
+
+import hibernate_test.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class Test2 {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration().configure().addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+
+        try{
+            Session session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+
+            List<Employee> list = session.createQuery("from Employee").getResultList();
+
+            for (Employee e: list) {
+                System.out.println(e);
+            }
+
+            session.getTransaction().commit();
+        }finally {
+            sessionFactory.close();
+        }
+    }
+}
